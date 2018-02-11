e5125ddb6c6ca564bb03522b80e4eeba8e93a2ec
Ross Beck-MacNeil <wertu234@gmail.com>
2018-02-11 15:53:49 -0500

    Changed to script from notebooks

--- src/helpers.py
+++ src/helpers.py
@@ -1,202 +1,223 @@
 import os
-import pickle
-import zipfile
-import glob
-import string
-import re
-import numpy as np
-from bs4 import BeautifulSoup
-from bs4.element import Comment
-
-import spacy
-
-from keras.layers import Layer
+from collections import deque
+from math import exp
+from keras.layers import Dense, Input, Masking, BatchNormalization, Layer, Embedding, Dropout
+from keras.layers import LSTM, Reshape, TimeDistributed, Concatenate, Multiply, RepeatVector
+from keras import Model
 from keras import backend as K
+import numpy as np
+import h5py
+import requests
+import string
+import pickle
+import pandas as pd
 
-
-from sklearn.externals import six
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.decomposition import NMF
-from sklearn.exceptions import NotFittedError
-
-#designed to encode a text to the averaged of its embeddings. Could also be used to return indexes of a texts  words
-#not certain
-class TextEncoder_old(object):
-    def __init__(self, embedding_dict, embedding_dims=None):
-        self.embedding_dict=embedding_dict
-        #simple pattern for tokens
-        #at least trow "word" characters
-        self.token_pattern = re.compile(r"(?u)\b\w\w+\b")
-        
-        #if the embedding dims are not supplied, infer from "first" item in dict
-        if embedding_dims is None:
-            self.embedding_dims = self.embedding_dict[list(self.embedding_dict.keys())[0]].shape[0]
-        else:
-            self.embedding_dims = embedding_dims
-
-        self.word_index = {key:i for i, key in enumerate(self.embedding_dict)}
-    def tokenize(self, text):
-        if not isinstance(text, (bytes, str)):
-            raise ValueError("text must be a string object!")
-        return self.token_pattern.findall(text.lower())
+def input_processer(seqs_path, dict_path):
+    jokes_url = "https://onedrive.live.com/download?cid=ABD51044F5341265&resid=ABD51044F5341265%21112436&authkey=AFUOwOJbFyY6ZFM"
+    jokes_path = "data/raw/reddit_jokes.csv"
     
-    def embed(self, text):
-        tokens = self.tokenize(text)
-        mean=np.zeros(self.embedding_dims, dtype=np.float32)
-        sum_wght=0
-        for token in tokens:
-            try:
-                vector = self.embedding_dict[token]
-                mean, sum_wght = self.__accum_mean(vector, mean, sum_wght)
-            except KeyError:
-                continue        
-        return mean
-    def __accum_mean(self, vec, prev_mean, prev_sum_wght):
-        new_sum_weight = prev_sum_wght+1
-        return (vec + prev_mean * (prev_sum_wght/new_sum_weight))/new_sum_weight, new_sum_weight
+    if not os.path.exists(jokes_path):
+        print("Jokes file not found, so downloading it.")
+        response = requests.get(jokes_url, allow_redirects=True)
+        with open(jokes_path, 'wb') as f:
+            f.write(response.content)
+        print("Jokes downloaded.")
 
-#should make a class that wraps the vecotrizer...
-class TextEncoder(object):
-    def __init__(self, vectorizer, max_features=2000):
-        self.vectorizer = vectorizer
-        self.dtm = None
-        self.max_features = max_features
-        
-    def __len__(self):
-        if self.dtm is not None:
-            return self.dtm.shape[0]
-        else:
-            raise NotFittedError("fit this instance first")
-    
-    def fit(self, docs, max_features=2000):
-        #if single string, make intpo tuple
-        if isinstance(docs, six.string_types):
-            docs = (docs,)
-        self.dtm = self.vectorizer.transform(docs)
-        return self.dtm
-    
-    def __getitem__(self, key):
-        wghts = np.zeros((self.max_features), dtype = np.float32)
-        feature_indices = np.zeros((self.max_features), dtype = np.int32)
-        
-        indices = np.random.permutation(np.arange(self.dtm.indptr[key],self.dtm.indptr[key+1]))
-        
-        last_feature = min(self.max_features, len(indices))
-        indices = indices[:last_feature]
-        feature_indices[:last_feature] = self.dtm.indices[indices]
-        wghts[:last_feature] = self.dtm.data[indices]
-        
-        return feature_indices, wghts
+    jokes = pd.read_csv(jokes_path)
+    #only funy ones
+    jokes = jokes[jokes["ups"]>=10]
 
-#USED TO CHECK IF THE TEXT IN HTML DOC IS VISIBLE
-def tag_visible(element):
-    if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
-        return False
-    if isinstance(element, Comment):
-        return False
-    return True
+    #get rid of joke with very little text
+    jokes = jokes[jokes["text"].str.len() >=4]
+    jokes = jokes["title"].str.cat(jokes["text"], sep="\n\n")
+    jokes = jokes.tolist()
+    jokes = np.array(jokes)
 
-##USED TO JOIN TEXT WITHIN PARAGRAP
-def concatenate_texts(texts):
-    return u"".join(t for t in texts)
+    #pre make dict
+    char_dict = {"<BOUND>":0}
+    for ix, char in enumerate(string.printable):
+        if char not in ('\x0c', '\x0b', "\r"):
+            char_dict[char] = ix+1
 
-#GETS TEXT FROM HTML
-#JOIN PARAGRAPHS TOGETHER WITH 2 NEWLINES (BREAKS)
-def text_from_html(html):    
-    soup = BeautifulSoup(html, "html.parser")
-    #first get paragraphs
-    paragraphs = soup.findAll("p")
-    #to exclude futenbeg headers
-    paragraphs = [paragraph.findAll(text=True) for paragraph in paragraphs]
-    paragraphs = [filter(tag_visible, paragraph) for paragraph in paragraphs]
-    paragraphs = [concatenate_texts(paragraph) for paragraph in paragraphs]
-    return u"\n\n".join(paragraph.strip() for paragraph in paragraphs)
-#Gets all html docs in a directory
-def get_chapters(directory, min_length=0):
-    chapters=[]
-    for file in os.listdir(directory):
-        filename = os.fsdecode(file)
-        if filename.endswith(".html"):
-            file_path=os.path.join(directory, filename)
-            chapter=text_from_html(file_path)
-            #filter out ones that are too short
-            if len(chapter) >= min_length:
-                chapters.append(chapter)
-            continue
-        else:
-            continue
-    return chapters
-#MEANT FOR EPUBS
-#EXTRACTS ALL HTML, HTM OR XHTML FILES
-#TREATS EACH SEPERATE FILE AS A SEPERATE CHAPER
-def chapters_from_archive(file, min_length=1000):
-    archive = zipfile.ZipFile(file, 'r')
-    files = [file for file in archive.namelist() if file.endswith((".htm","html","xhtml"))]
-    chapters=[]
-    for file in files:
-        html=archive.read(file)
-        chapter=text_from_html(html)
-        #filter out ones that are too short
-        if len(chapter) >= min_length:
-            chapters.append(chapter)
-            continue
-        else:
-            continue
-    return chapters
+    #now save the character dict
+    pickle_out = open(dict_path,"wb")
+    pickle.dump(char_dict, pickle_out)
+    pickle_out.close()
 
-class WghtdAverage(Layer):
-    def __init__(self, **kwargs):
-        super(WghtdAverage, self).__init__(**kwargs)
-        self.supports_masking = True
+    f = h5py.File(seqs_path, 'w')
+    dt = h5py.special_dtype(vlen=np.dtype('int32'))
+    dset = f.create_dataset('seqs', (len(jokes),), dtype=dt)
+    for i in range(len(jokes)):
+        text=jokes[i]
+        #make certain all strings
+        seq = [char_dict["<BOUND>"]]
+        for char in text:
+            if char in string.printable:
+                try:
+                    seq.append(char_dict[char])
+                except KeyError:
+                    pass
+        dset[i]=np.array(seq, dtype="int32")
 
-    def compute_output_shape(self, input_shape):
-        return (input_shape[0][0], input_shape[0][-1])
-    def compute_mask(self, input, input_mask=None):
-        # do not pass the mask to the next layers
-        return None
-    
-    def call(self, inputs, mask = None):
-        #num willl have dimensions: dimension batch_size X embedding_dims X 1
-        num = inputs[0] * inputs[1]
-        return K.sum(num, axis=1) / (K.sum(inputs[1], axis=1) + K.epsilon())
+
 
 def sparse_softmax_cross_entropy_with_logits (y_true, y_pred):
     return K.sparse_categorical_crossentropy(target=y_true, output=y_pred, from_logits=True)
+    #check if the jokes file is available, and download it if it is not
 
 
-class LemmaTokenizer(object):
-    def __init__(self):
-        self.nlp = spacy.load("en")
-    def __only_letters(self, string):
-        return re.match("^[a-z]+$", string) != None
-    def __call__(self, doc):
-         return [token.lemma_ for token in self.nlp(doc) if self.__only_letters(token.lemma_)]
+#class for iterating over jokes
+class CharGenSequence(object):
+    def __init__(self, seqs, char_dict, batch_size=500, seq_length=100):
+        self.seqs = seqs
+        self.char_dict = char_dict
+        #need to know how big input to neural net (length of sequnece)
+        self.seq_length = seq_length
+        self.batch_size = batch_size
+        #the  random permtuion returns a randomly sorted rangs
+        self.seq_idxs = deque(np.random.permutation(len(seqs)).tolist())
+        #now intitilize first batch
+        #these are the indexes of seq_list that are used for the batch.
+        #queu will help, can pop from left
+        self.batch_idxs = [self.seq_idxs.pop() for _ in range(self.batch_size)]
+        #walyas start at gegininb of joke
+        self.seq_pos = [0 for seq in self.batch_idxs]
+        
+    def __next__(self):
+        #make masks
+        #used to determine if will use reset state or not...
+        #will rely on brtaod casting to gie ii th eproer shape
+        state_mask = np.ones((self.batch_size, 1), dtype=np.float32)
+        #make x, the input a numpy array of zeros (initilaly)
+        #nowing providing just indexes, since
+        x = np.zeros((self.batch_size, self.seq_length, len(self.char_dict)), dtype=np.float)
+        
+        #will use sparse categorical
+        y = np.zeros((self.batch_size, self.seq_length, 1), dtype=np.int32)
+        
+        #LOOP OVER BATCH
+        #seq_idx is the index of the sequnce within seq_list
+        #while batch_idx is the index of the sequence within the batch        
+        for batch_idx, seq_idx in enumerate(self.batch_idxs):
+            #work fowards...
+            #GO UP TO LENGTH OF OUTPUTS...
+            #check if this will be last batch for this input seq
 
+            for pos_idx in range(self.seq_length):
+                #self.seq_pos is start of sequence
+                input_pos_idx =  self.seq_pos[batch_idx]+pos_idx
+                #OUTPUTS ALWAYS ONE AHEAD OF INPUTS
+                output_pos_idx = input_pos_idx + 1
+                #if desired index does not exit, leave blank....
+                try:
+                    x[batch_idx, pos_idx, self.seqs[seq_idx][input_pos_idx]] = 1.
+                except IndexError:
+                    #leave at default of 0 (padding value)
+                    pass
+                try:
+                    y[batch_idx, pos_idx, 0] = self.seqs[seq_idx][output_pos_idx]
+                except IndexError:
+                    #will be masked anyways?
+                    y[batch_idx, pos_idx, 0] = self.char_dict["<BOUND>"]
+        
+            #DO SPECIAL STUFF IF length of sequence is less than than what was desired...
+            if len(self.seqs[seq_idx]) <= (self.seq_length + self.seq_pos[batch_idx]):
+                #first add back to avaialbe
+                self.seq_idxs.append(self.batch_idxs[batch_idx])
+                
+                #pop left ensures first in, first out
+                self.batch_idxs[batch_idx] = self.seq_idxs.popleft()
+                    
+                #set start pos back to 0
+                self.seq_pos[batch_idx] = 0
+                
+                #make masks = 0 to reset state
+                #could probaly do this outside of the generator...
+                state_mask[batch_idx,0] = 0.0
+            else:
+                #increment position by seq_length
+                #want last output character to be the first input character...
+                self.seq_pos[batch_idx]=self.seq_pos[batch_idx]+ self.seq_length
+        return(x, y, state_mask)
+    def __iter__(self):
+        return self
 
-def only_letters(string):
-    return re.match("^[a-z]+$", string) != None
+def create_model(batch_size, input_length, num_chars, batch_momentum=0.99, rnn_depth=1, dropout=0.0):
+    #character sequences
+    character_input = Input(batch_shape=(batch_size,input_length, num_chars), dtype='float', name='char_indx_input')
+    sequences = Masking(name="mask")(character_input)
 
-nlp = spacy.load("en")
-def lemma_tokenize(doc):
-    return [token.lemma_ for token in nlp(doc) if only_letters(token.lemma_)]
+    for i in range(rnn_depth):
+        units = 128 * (2 ** i)
+        rnn =  LSTM(units=units,return_sequences=True, stateful=True,
+                    dropout=dropout, recurrent_dropout=dropout, name="rnn"+str(i))(sequences)
+        #don't use batch normaliztion on last layer
+        if i == (rnn_depth-1):
+            sequences = rnn
+        else:
+            sequences = BatchNormalization(momentum=batch_momentum, name="normalize"+str(i))(rnn)
+    drop = TimeDistributed(Dropout(dropout), name="dropout")(rnn)
+    preds = TimeDistributed(Dense(num_chars), name="logits")(drop)
+    model = Model(inputs=[character_input], outputs=preds)
+    
+    return model
 
+def reset_states(model, layer, mask):
+    states = model.get_layer(layer)._states
+    states = [np.multiply(K.eval(state), mask) for state in states]
+    model.get_layer(layer).reset_states(states)
 
-class TopicModeler(object):
-    def __init__(self, n_topics=32):
-        self.vectorizer = TfidfVectorizer(tokenizer=lemma_tokenize, norm="l1", strip_accents="ascii",
-                             lowercase=False, use_idf=True, stop_words="english", min_df=5, max_df=0.9)
-        self.model = NMF(n_topics)
-    def fit_transform(self, texts):
-        dtm = self.vectorizer.fit_transform(texts)
-        return self.model.fit_transform(dtm)
+class GenerateJoke(object):
+    def __init__(self, char_dict, max_len=1000, base_temp=1.0, temp_decay=0.001):
+        #reverse the character dictionary, since will need to go from integer index to character
+        self.char_dict_reverse = {value:key for key, value in char_dict.items()}
+        self.char_dict = char_dict
+        #keep track of last character
+        self.end_ix = char_dict["<BOUND>"]
+        self.max_len = max_len
+        self.base_temp = base_temp
+        self.temp_decay = temp_decay
 
-    def transform(self, texts):
-        dtm = self.vectorizer.transform(texts)
-        return self.model.transform(dtm)
-    def top_words(self, n_top_words = 20):
-        feature_names = self.vectorizer.get_feature_names()
-        top_words = []
+    def __sample(self, preds, temperature=1.0):
+        preds = np.asarray(preds).astype('float64')
+        #is logged proability
+        #so exp(log(prob) / temperature) is smaller when temperature is higer
+        #however derivative respect to temp: -log(prob) /temperature^2
+        #-log(prob) is bigger when prob is smaller
+        #so result is that lower temp makes smaller probs go to 0 faster
+        preds = preds / temperature 
+        exp_preds = np.exp(preds)
+        preds = exp_preds / np.sum(exp_preds)
 
-        for topic_idx, topic in enumerate(self.model.components_):
-            top_words.append([feature_names[i] for i in topic.argsort()[:-n_top_words - 1:-1]])
-        return top_words
+        probas = np.random.multinomial(1, preds, 1)
+        return np.argmax(probas)   
+
+    def __call__(self, model):
+        #need to make certain hiddent states are 0
+        model.reset_states()
+
+        x_input = np.zeros((1,1, len(self.char_dict)), dtype =np.float32)
+        #first input to the model will be the <BOUND> token
+        x_input[0,0, self.end_ix] = 1.0
+        #x_indxs is used to store output, ant thus to generate jokes
+        x_indxs = []
+
+        for i in range(self.max_len):
+            #want a decreasing temperature
+            temperature = max(0.1, self.base_temp*exp(-i*self.temp_decay))
+            #want only first...
+            preds = model.predict_on_batch(x={"char_indx_input":x_input})[0,0]
+            next_index = self.__sample(preds, temperature=temperature)
+            #only need to update first index, since stateful...
+            #make x_input again
+            x_input = np.zeros((1,1, len(self.char_dict)), dtype =np.float32)
+            x_input[0,0,next_index] = 1.0
+            #now append to list that is used for text genration...
+            if next_index == self.end_ix:
+                break
+            else:
+                x_indxs.append(next_index)
+        x_tokens = [self.char_dict_reverse[indx] for indx in x_indxs]
+        x_string = "".join(x_tokens)
+        return(x_string)
+
---
+++ src/train.py
@@ -0,0 +1,83 @@
+import os
+import sys
+import pickle
+import h5py
+from keras.optimizers import Nadam
+import numpy as np
+from tqdm import tqdm
+import helpers 
+
+
+CHARS_SEQS_PATH = "data/processed/char_sequences.h5"
+CHAR_DICT_PATH = "data/processed/char_dict.pkl"
+BATCH_SIZE=512
+RNN_DEPTH = 3
+SEQ_LENGTH = 300
+TRAINING_MODEL_PATH = "models/joke_gen_rnn.h5"
+PREDICT_MODEL_PATH = "models/joke_gen_rnn_predict.h5"
+BATCHES_PER_EPOCH = 250
+DROPOUT_R = 0.1
+NUM_EPOCHS = 1
+
+if __name__ == "__main__":
+    os.chdir(os.path.join(__file__, os.pardir, os.pardir))
+
+    if not os.path.exists(CHARS_SEQS_PATH):
+        print("Processed input not found. Jokes will be downloaded and processed")
+        helpers.input_processer(seqs_path=CHARS_SEQS_PATH, dict_path=CHAR_DICT_PATH)
+
+    h5f = h5py.File(CHARS_SEQS_PATH, "r")
+    seqs = h5f["seqs"][:]
+    h5f.close()
+    print("Loaded sequences")
+    #load char dict
+    pickle_in = open(CHAR_DICT_PATH,"rb")
+    char_dict = pickle.load(pickle_in)
+    pickle_in.close()
+    num_seqs = seqs.shape[0]
+    print("There are {} jokes in total.".format(num_seqs))
+    batch_momentum = 1 - BATCH_SIZE/num_seqs
+    num_chars=len(char_dict)
+    gen_seq =  helpers.CharGenSequence(seqs, char_dict=char_dict, seq_length=SEQ_LENGTH, batch_size=BATCH_SIZE)
+
+    if os.path.exists(TRAINING_MODEL_PATH):
+        print("Model already exists. If you continue this model will be ovewritten. Type 'yes' to continue. "
+              "Type 'no' to exit and keep existing model.")
+        resume = ""
+        while resume not in ("yes", "no"):
+            resume = input("yes/no: ")
+        if resume == "no":
+            sys.exit()
+        else:
+            print("Starting training from scratch. Existing model will be overwritten.")
+    training_model = helpers.create_model(batch_size=BATCH_SIZE, input_length=SEQ_LENGTH, num_chars=num_chars,
+                                              batch_momentum=batch_momentum, rnn_depth=RNN_DEPTH, dropout=DROPOUT_R)
+    training_model.compile(loss=helpers.sparse_softmax_cross_entropy_with_logits, optimizer=Nadam())
+    print("Model compiled and ready for training. Here is its summary:")
+    print(training_model.summary())
+    predict_model = helpers.create_model(batch_size=1, input_length=1, num_chars=num_chars,
+                             rnn_depth=RNN_DEPTH)
+
+    generator = helpers.GenerateJoke(char_dict)
+    #now a loop
+    epoch = 0
+    while epoch < NUM_EPOCHS:
+        print("***** Epoch {} *****".format(epoch)) 
+        loss = np.zeros(BATCHES_PER_EPOCH, dtype=np.float32)
+        for i in tqdm(range(BATCHES_PER_EPOCH)):
+            char_input, y, state_mask = next(gen_seq)
+            loss[i] = training_model.train_on_batch(x={"char_indx_input":char_input}, y=y)
+            #now masking bit...
+            for i in range(RNN_DEPTH):
+                helpers.reset_states(training_model, "rnn"+str(i), state_mask)
+        #save model, in case training is interupted
+        training_model.save(PREDICT_MODEL_PATH)
+        #lower learning rate after every "epoch"
+        print("Average loss of {:.4f}".format(np.mean(loss)))
+        print("***** EXAMPLE OUTPUT*****")
+        predict_model.set_weights(training_model.get_weights())
+        joke = generator(predict_model)
+        print(joke)
+        epoch+=1
+    #save final predict model
+    predict_model.save(PREDICT_MODEL_PATH)
